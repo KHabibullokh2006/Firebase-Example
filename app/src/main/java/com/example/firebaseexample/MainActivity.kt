ab4f1897c6e0a5e4f4958ea22c8d2a60d4b0cc57
User_7 <khabibullokh2006@gmail.com>
2023-12-13 09:58:30 +0500

    Comment

--- app/src/main/java/com/example/firebaseexample/MainActivity.kt
+++ app/src/main/java/com/example/firebaseexample/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.firebaseexample
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -17,21 +19,33 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.firebaseexample.ui.theme.FirebaseExampleTheme
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.database.database
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
     private lateinit var auth: FirebaseAuth
-
+    val database = Firebase.database
+    val myRef = Firebase.database.reference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FirebaseExampleTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     auth = FirebaseAuth.getInstance()
+                    val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.client_id))
+                        .requestEmail()
+                        .build()
+                    val mGoogleSignInClient = GoogleSignIn.getClient(this,gso)
                     Column(
                         modifier = Modifier
                             .fillMaxSize()
@@ -39,10 +53,15 @@ class MainActivity : ComponentActivity() {
                         horizontalAlignment = Alignment.CenterHorizontally,
                         verticalArrangement = Arrangement.Center
                     ) {
-                        Button(onClick = { /*TODO*/ }) {
+                        Button(onClick = {
+                            val signInIntent = mGoogleSignInClient.signInIntent
+                            startActivityForResult(signInIntent,1)
+                        }) {
                             Text(text = "Sign In with Google")
                         }
-                        Button(onClick = { /*TODO*/ }) {
+                        Button(onClick = {
+                            mGoogleSignInClient.signOut()
+                        }) {
                             Text(text = "Sign Out")
                         }
                     }
@@ -50,6 +69,44 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account.idToken)
+                Log.d("TAG", "onActivityResult: ")
+            }catch (e:ApiException){
+                Log.d("TAG", "error: $e")
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken,null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this){task ->
+                if (task.isSuccessful){
+                    val user = auth.currentUser
+                    Log.d("User", user?.displayName.toString())
+                    val userData = User(user?.displayName,user?.uid, user?.photoUrl.toString())
+                    myRef.child("contact").child(user?.uid?: "")
+                        .setValue(userData)
+                        .addOnSuccessListener {
+                            val i  = Intent(this,ContactActivity::class.java)
+                            i.putExtra("name",userData.name)
+                            startActivity(i)
+                        }
+                }else{
+                    Log.d("TAG", "firebaseAuthWithGoogle: Task Unsuccessful")
+                }
+            }
+    }
+
 }
 
 @Composable
---
+++ app/src/main/java/com/example/firebaseexample/User.kt
@@ -0,0 +1,3 @@
+package com.example.firebaseexample
+
+data class User(val name:String?, val uid:String?, val photo:String?)
