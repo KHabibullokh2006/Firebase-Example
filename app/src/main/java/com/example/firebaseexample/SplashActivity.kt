36053aa932ee586bffaa00cd745bf94b1997dd2e
User_7 <khabibullokh2006@gmail.com>
2023-12-19 11:26:51 +0500

    comment

--- app/src/main/java/com/example/firebaseexample/SplashActivity.kt
+++ app/src/main/java/com/example/firebaseexample/SplashActivity.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.firebaseexample.ui.theme.FirebaseExampleTheme
 
 @SuppressLint("CustomSplashScreen")
